0bf6cf4d09d7a11bb9a9e3b386d286db6b08c1b2
RastogiTanya <rastogitanya96@gmail.com>
2021-02-20 14:53:08 +0530

    Comments added

--- App.js
+++ App.js
@@ -1,31 +1,33 @@
 console.log("welcome");
-
+showNotes(); //Displaying notes as soon as the page opens 
 let addBtn = document.getElementById("addBtn");
-
+//Adding a note
 addBtn.addEventListener("click", function (e) {
     let addTxt = document.getElementById("addTxt");
     let notes = localStorage.getItem("notes");
-    if (notes==null){
-        notesObj=[];
+    if (notes == null) {
+        notesObj = [];
     }
-    else{
-        notesObj=JSON.parse(notes);
+    else {
+        notesObj = JSON.parse(notes);
     }
     notesObj.push(addTxt.value);
     localStorage.setItem("notes", JSON.stringify(notesObj));
-    addTxt.value="";
+    addTxt.value = "";
     showNotes();
 })
-function showNotes(){
+//Function to display notes as soon as they are added
+function showNotes() {
     let notes = localStorage.getItem("notes");
-    if (notes==null){
-        notesObj=[];
+    if (notes == null) {
+        notesObj = [];
     }
-    else{
-        notesObj=JSON.parse(notes);
+    else {
+        notesObj = JSON.parse(notes);
     }
-    let html="";
-    notesObj.forEach(function(element,index){
+    let html = "";
+    //Adding the card
+    notesObj.forEach(function (element, index) {
         html += `
         <div class="mx-2 my-2 card " style="width: 18rem;">
         <div class="card-body">
@@ -35,11 +37,13 @@ function showNotes(){
         </div>                
     </div>
         `;
-    }); 
-    let notesElem=document.getElementById("notes");
-    if(notes.length!=0)
-    {
-        notesElem.innerHTML=html;
+    });
+    let notesElem = document.getElementById("notes");
+    if (notesObj.length != 0) {
+        notesElem.innerHTML = html;
     }
-
+    else {
+        notesElem.innerHTML = `No notes here.`;
+    }
 }
+//finction to delete the nodes
